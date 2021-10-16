7bc6b59d970a22bc1a88d308c561d369493ac35e
NatanaelRazzotto <ana@outlook.com>
2021-10-15 23:50:36 -0300

    Incluindo validacao de persistencia da entidade Result

--- src/server/repository/sequelize/repositoryClass/repositoryResults.js
+++ src/server/repository/sequelize/repositoryClass/repositoryResults.js
@@ -1,9 +1,38 @@
 const { ModelResults } = require('../model/modelResults');
+const { Op } = require("sequelize");
 
 class RepositoryResults {
-    async create(races) {
-        const received = await ModelResults.create(races);
-        return received.dataValues;
+    async create(result) {
+        const validate = await this.findAllWhere(result);
+        if ((validate[0] != null)) {
+            console.log("já tem");
+            //  console.log(validate);
+            return validate[0];
+        }
+        else {
+            console.log("não tem");
+            const received = await ModelResults.create(result);
+            return received.dataValues;
+        }
+    }
+
+    async findAllWhere(resultObject) {
+        const Results = await ModelResults.findAll({
+            where: {
+                [Op.and]: [
+                    { raceId: resultObject.raceId },
+                    { driverId: resultObject.driverId },
+                    { constructorId: resultObject.constructorId },
+                    { number: resultObject.number }
+                ]
+            },
+            raw: true,
+            limit: 1
+        }).then(function (result) {
+            console.log(" test + " + result);
+            return result;
+        });
+        return Results;
     }
 
     async findAll() {
