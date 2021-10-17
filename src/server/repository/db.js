d51c0f13f7272424793db7bd752780eb167156e2
razzotto <natanael8447@outlook.com>
2021-10-17 13:27:57 -0400

    Inclusao de docker-compose para nodejs e mysql

--- src/server/repository/db.js
+++ src/server/repository/db.js
@@ -7,9 +7,15 @@ const {
 // const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
 const sequelize = new Sequelize(database, username, password, {
   dialect: "mysql",
-  host: "127.0.0.1",
+  host: "db",//"127.0.0.1"
+  port: 3306,
   logging: false,
 });
+
+sequelize.sync({ alter: true })
+  .then(() => {
+    console.log(`Database sync & tables created and update!`)
+  })
 
 module.exports = sequelize;
 
